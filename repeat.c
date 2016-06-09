8925a56dce261340b133beb27ef7678f9f67b872
Rojapreetha <rojapreetha95@gmail.com>
2016-06-09 11:46:06 +0530

    Create repeat.c

---
+++ repeat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,i,k,max=0;
+	scanf("%d",&t);
+	int a[t],h[1000];
+       for(i=0;i<1000;i++){
+h[i]=0;
+}
+	for(i=0;i<t;i++)
+	{
+		scanf("%d",&a[i]);
+		k=a[i];
+		h[k]++;
+         if(max<a[i])
+            max=a[i];
+}
+	for(i=0;i<=max;i++)
+	{
+		if(h[i]>1)
+		{
+		 printf(" %d",i);
+               h[i]=0;	
+	        }
+	}
+	return 0;
+}
